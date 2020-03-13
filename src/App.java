1b82dfca020bdd4d692566f6cfb6348f15f5dce6
okapifan <wxzegers@gmail.com>
2020-03-13 12:47:42 +0100

    Fixed strange bug

--- src/App.java
+++ src/App.java
@@ -1,6 +1,8 @@
+import javax.swing.SwingUtilities;
+
 public class App {
 	public static void main(String[] args) {
-		//SwingUtilities.invokeLater(MainScreen::new);
+		SwingUtilities.invokeLater(MainScreen::new);
 	}
 
 }
