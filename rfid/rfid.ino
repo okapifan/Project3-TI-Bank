ee810c5f14164da53c16c9ec1b853eb4c5969525
Robbink <robbinkoot@gmail.com>
2020-03-23 11:05:27 +0100

    Create rfid.ino
    de code voor de RFID naar de seriele monitor

---
+++ rfid/rfid.ino
@@ -0,0 +1,54 @@
+#include <SPI.h>
+#include <MFRC522.h>
+ 
+#define SS_PIN 10
+#define RST_PIN 9
+MFRC522 mfrc522(SS_PIN, RST_PIN);
+ 
+void setup() 
+{
+  Serial.begin(9600);
+  SPI.begin();
+  mfrc522.PCD_Init();
+  Serial.println("card please");
+  Serial.println();
+
+}
+void loop() 
+{
+  // Look for new cards
+  if ( ! mfrc522.PICC_IsNewCardPresent()) 
+  {
+    return;
+  }
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) 
+  {
+    return;
+  }
+  //Show UID on serial monitor
+  Serial.print("UID tag :");
+  String content= "";
+  byte letter;
+  for (byte i = 0; i < mfrc522.uid.size; i++) 
+  {
+     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+     Serial.print(mfrc522.uid.uidByte[i], HEX);
+     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+     content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  Serial.println();
+  Serial.print("Message : ");
+  content.toUpperCase();
+  if (content.substring(1) == "EA FB E4 0B")
+  {
+    Serial.println("Authorized access");
+    Serial.println();
+    delay(3000);
+  }
+ 
+ else   {
+    Serial.println("NOPE");
+    delay(3000);
+  }
+} 
