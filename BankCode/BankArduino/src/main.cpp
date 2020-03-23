3e3dffc3d2e1c7d9390c1e05bef6493551ee7d9b
Daniël van der Drift <danielvdd1@hotmail.com>
2020-03-23 11:34:13 +0100

    Arduino serial communication code

--- BankCode/BankArduino/src/main.cpp
+++ BankCode/BankArduino/src/main.cpp
@@ -1,9 +1,60 @@
 #include <Arduino.h>
 
+
+// Wait without delay
+unsigned long previousMillis = 0;
+const long interval = 4000;
+
+
+// Functions
+void SendString(String data);
+char* ReceiveString();
+
+
+
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Send a string
+  SendString("Send");
+
+
+  // Receive a string
+  char* userInput = ReceiveString();
+  if (userInput!=NULL)
+  {
+    // Do something with the received string
+
+    // Send the string back
+    SendString(userInput);
+  }
+}
+
+
+
+void SendString(String data){
+  Serial.print(data);
+}
+
+char* ReceiveString()
+{
+  static char data[21]; // For strings of max length=20
+  if (!Serial.available()) return NULL;
+  delay(64); // wait for all characters to arrive
+  memset(data,0,sizeof(data)); // clear data
+  byte count=0;
+  while (Serial.available())
+  {
+    char c=Serial.read();
+    if (c>=32 && count<sizeof(data)-1)
+    {
+      data[count]=c;
+      count++;
+    }
+  }
+  data[count]='\0'; // make it a zero terminated string
+  return data;
 }
