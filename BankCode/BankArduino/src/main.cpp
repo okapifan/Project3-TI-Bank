5623195ec68c13e0676180b9429d763bf2296b15
Timovdmeer115 <Timovdmeer115@gmail.com>
2020-03-23 14:11:52 +0100

    Keypad update

--- BankCode/BankArduino/src/main.cpp
+++ BankCode/BankArduino/src/main.cpp
@@ -1,10 +1,30 @@
 #include <Arduino.h>
 #include <SPI.h>
 #include <MFRC522.h>
+#include <Keypad.h>
 
 #define SS_PIN 10
 #define RST_PIN 9
 MFRC522 mfrc522(SS_PIN, RST_PIN);
+
+// Defining the keypad
+const byte ROWS= 4;
+
+const byte COLS= 4;
+
+char keys[ROWS][COLS]= {
+{'1', '2', '3', 'A'},
+{'4', '5', '6', 'B'},
+{'7', '8', '9', 'C'},
+{'*', '0', '#', 'D'}
+};
+
+byte rowPins[ROWS] = {2,3,4,5}; //Rows 0 to 3
+
+byte colPins[COLS]= {6,7,8,9}; //Columns 0 to 3
+
+//initializes an instance of the Keypad class
+Keypad keypad= Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
 // Wait without delay
 unsigned long previousMillis = 0;
@@ -23,6 +43,12 @@ void setup()
 
 void loop()
 {
+	char key = keypad.getKey();
+
+	if (key){
+		void SendString(Key);
+	}
+
   //look for new cards
   if (mfrc522.PICC_IsNewCardPresent())
   {
