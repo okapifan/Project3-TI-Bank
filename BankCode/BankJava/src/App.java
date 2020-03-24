9554cd3bf81c1adda2867fcf39389c389b608b27
okapifan <wxzegers@gmail.com>
2020-03-24 22:02:42 +0100

    Added Functionality for pressing the keypad to change screens

--- BankCode/BankJava/src/App.java
+++ BankCode/BankJava/src/App.java
@@ -1,49 +1,66 @@
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
 import javax.swing.SwingUtilities;
 import com.fazecast.jSerialComm.*;
 
 public class App {
 
+	public static MainScreen ms;
+	
     public static void main(String[] args) throws IOException {
-        SerialPort comPort = SerialPort.getCommPorts()[0];
-		comPort.openPort();		
-		SwingUtilities.invokeLater(MainScreen::new); //create Mainscreen
+        
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				SerialPort comPort = SerialPort.getCommPorts()[0];
+				comPort.openPort();
+				ms = new MainScreen(); //create Mainscreen
+				
+				try {
+					//while (true) // Loop through te code
+					//{
+						// Receive string
+						if (comPort.bytesAvailable() > 0) {
+							byte[] readBuffer = new byte[comPort.bytesAvailable()];
+							//comPort.readBytes(readBuffer, readBuffer.length);
+							int dataSize = comPort.readBytes(readBuffer, readBuffer.length);
+							String data = new String(readBuffer);
 
-        try {
-            while (true) // Loop through te code
-            {
-                // Receive string
-                if (comPort.bytesAvailable() > 0) {
-                    byte[] readBuffer = new byte[comPort.bytesAvailable()];
-                    //comPort.readBytes(readBuffer, readBuffer.length);
-                    int dataSize = comPort.readBytes(readBuffer, readBuffer.length);
-                    String data = new String(readBuffer);
+							// Use data
+							System.out.println("Read data: " + data + " (" + dataSize + " bytes)");
 
-                    // Use data
-                    System.out.println("Read data: " + data + " (" + dataSize + " bytes)");
-
-                    // Call function to parse the data
-                }
-
-
-                // Simulate event
-                if (System.in.available() > 0){
-                    while(System.in.available() > 0){
-                        System.in.read();
-                    }
-
-
-                    // Send String
-                    String data = "Test";
-                    comPort.writeBytes(data.getBytes(), data.length());
-                }
-
-                
-            }
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-        comPort.closePort();
+							// Call function to parse the data
+							if(ms.currentScreen == 0) { //homescreen
+								if(data.equals("1")) {
+									ms.switchToBalanceScreen();
+									ms.currentScreen = 1;
+								}
+							} else { //balancescreen
+								if(data.equals("1")) {
+									ms.switchToHomeScreen();
+									ms.currentScreen = 0;
+								}
+							}
+						}
+						
+						// Simulate event
+						if (System.in.available() > 0){
+							while(System.in.available() > 0){
+								System.in.read();
+							}
+							
+							// Send String
+							String data = "Test";
+							comPort.writeBytes(data.getBytes(), data.length());
+						}
+						//TimeUnit.SECONDS.sleep(60);
+					//}
+				}
+				catch (Exception e) {
+					e.printStackTrace();
+				}
+				comPort.closePort();
+			}
+		});      
     }
 }
