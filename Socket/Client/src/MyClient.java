514673b468abfb49ea33105da2816f367cea50a2
Danielvdd1 <44206022+Danielvdd1@users.noreply.github.com>
2020-05-17 13:30:49 +0200

    Added Socket code

---
+++ Socket/Client/src/MyClient.java
@@ -0,0 +1,24 @@
+import java.net.*;
+import java.io.*;
+public class MyClient{
+    public static void main(String args[])throws Exception{
+        Socket s=new Socket("145.24.222.211",8000);
+        DataInputStream din=new DataInputStream(s.getInputStream());
+        DataOutputStream dout=new DataOutputStream(s.getOutputStream());
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+
+        String str="",str2="";
+        while(!str.equals("stop")){
+            // Receive
+            str=br.readLine();
+            dout.writeUTF(str);
+            dout.flush();
+
+            //Send
+            str2=din.readUTF();
+            System.out.println("Server says: "+str2);
+        }
+
+        dout.close();
+        s.close();
+    }}
