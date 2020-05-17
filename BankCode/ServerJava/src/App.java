40392f6d8f135b96c0e261fcfacf3111ce84d288
Danielvdd1 <44206022+Danielvdd1@users.noreply.github.com>
2020-05-17 14:15:29 +0200

    Created JavaServer code

---
+++ BankCode/ServerJava/src/App.java
@@ -0,0 +1,14 @@
+/*
+ * Project 3/4
+ * 
+ * Daniël van der Drift
+ * Robbin Koot
+ * Timo van der Meer
+ * Zoë Zegers
+ */
+
+ public class App {
+	public static void main(String[] args) {
+		
+	}
+}
